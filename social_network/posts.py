608303268f06d20eb1740a16d36cdb8a5efa92d1
julysses <julyssesd@gmail.com>
2018-12-17 18:05:47 +0000

    Whew

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,41 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.timestamp = timestamp
+        self.user = None
+
+    def set_user(self, user):
+        self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):
+    def __init__(self, text, timestamp=None):
+        super().__init__(text,timestamp)
+        
+    def __str__(self):
+        return '@{first} {last}: "{text}"\n\t{date}'.format(first=self.user.first_name, last=self.user.last_name, text=self.text, date=self.timestamp.strftime("%A, %b %d, %Y"))
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+        super().__init__(text,timestamp)
+        self.image_url = image_url
+      
+
+    def __str__(self):
+        return '@{first} {last}: "{text}"\n\t{img}\n\t{date}'.format(first=self.user.first_name, last=self.user.last_name, text=self.text, img=self.image_url, date=self.timestamp.strftime("%A, %b %d, %Y"))
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super().__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+
+
+    def __str__(self):
+        return '@{first} Checked In: "{text}"\n\t{lat}, {log}\n\t{date}'.format(first=self.user.first_name, text=self.text, lat=self.latitude,log=self.longitude, date=self.timestamp.strftime("%A, %b %d, %Y"))
+
